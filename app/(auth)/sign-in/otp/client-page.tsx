1b30dcb4337dc27c670b800d48cf43d1ea15cc15
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-03-11 18:45:37 +0200

    Update client-page.tsx

--- app/(auth)/sign-in/otp/client-page.tsx
+++ app/(auth)/sign-in/otp/client-page.tsx
@@ -1,22 +1,35 @@
 "use client";
 import { OTPForm } from "@/app/(auth)/sign-in/otp/otp-form";
 import { ResendOTP } from "@/app/(auth)/sign-in/otp/resend-otp";
+import { Loader2 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from 'react'
+import { Suspense } from "react";
 
-function SearchBarFallback() {
-  return <>placeholder</>
-}
-
-export function ClientOTPPage() {
+function OTPContent() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email") || "";
+
   return (
-    <Suspense fallback={<SearchBarFallback />}>
-    <div className="flex flex-col justify-center items-center p-4 ">
+    <div className="flex flex-col justify-center items-center p-4">
       <OTPForm email={email} />
       <ResendOTP email={email} />
     </div>
+  );
+}
+
+export function ClientOTPPage() {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <span className="flex items-center text-sm">
+            <Loader2 className="animate-spin mr-2" />
+            Loading...
+          </span>
+        </div>
+      }
+    >
+      <OTPContent />
     </Suspense>
   );
 }

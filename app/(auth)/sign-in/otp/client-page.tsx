882a7d48df8aff2833d7f0ddc3da5f0e9c510aea
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-03-11 18:28:34 +0200

    Update client-page.tsx

--- app/(auth)/sign-in/otp/client-page.tsx
+++ app/(auth)/sign-in/otp/client-page.tsx
@@ -2,14 +2,21 @@
 import { OTPForm } from "@/app/(auth)/sign-in/otp/otp-form";
 import { ResendOTP } from "@/app/(auth)/sign-in/otp/resend-otp";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from 'react'
+
+function SearchBarFallback() {
+  return <>placeholder</>
+}
 
 export function ClientOTPPage() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email") || "";
   return (
+    <Suspense fallback={<SearchBarFallback />}>
     <div className="flex flex-col justify-center items-center p-4 ">
       <OTPForm email={email} />
       <ResendOTP email={email} />
     </div>
+    </Suspense>
   );
 }

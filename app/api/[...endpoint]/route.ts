6c41209e4cbf4c9a1906b1e31fc9818bea4042e8
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-03-11 19:07:58 +0200

    Update route.ts

--- app/api/[...endpoint]/route.ts
+++ app/api/[...endpoint]/route.ts
@@ -1,6 +1,8 @@
 import { app } from '@/api';
 import { handle } from 'hono/vercel';
 
+export const runtime = 'edge'
+
 export const OPTIONS = handle(app);
 export const GET = handle(app);
 export const POST = handle(app);

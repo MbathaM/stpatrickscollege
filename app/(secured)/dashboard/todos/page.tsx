776f40ead9abd078be2112f919b8bcd34d33f504
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-03-25 11:26:01 +0200

    Update page.tsx

--- app/(secured)/dashboard/todos/page.tsx
+++ app/(secured)/dashboard/todos/page.tsx
@@ -1,6 +1,10 @@
 
+import { api } from '@/convex/_generated/api';
+import { preloadedQueryResult, preloadQuery } from 'convex/nextjs';
 import TodosPageClient from './page-client';
 
-export default function TodosPage() {
- return <TodosPageClient />
+export default async function TodosPage() {
+    const todos = preloadedQueryResult(await preloadQuery(api.todo.list));
+    const subjects = preloadedQueryResult(await preloadQuery(api.subject.list));
+ return <TodosPageClient subjects={subjects} todos={todos}/>
 }

24abcbd66079c5cadf4b448806db0fb71ef06d43
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-03-25 11:25:42 +0200

    Update page.tsx

--- app/(secured)/dashboard/comment/page.tsx
+++ app/(secured)/dashboard/comment/page.tsx
@@ -1,6 +1,11 @@
 import { getAdUsers } from "@/helpers/get-ad-user";
 import { AzureUser } from "@/types";
 import { CommentForm } from "./comment-form";
+import { getGrades, getSubjects } from "@/helpers/get-grade_subjects";
+import { api } from "@/convex/_generated/api";
+import { auth } from "@/utils/auth";
+import { preloadedQueryResult, preloadQuery } from "convex/nextjs";
+import { headers } from "next/headers";
 
 async function getAzureUsers(): Promise<AzureUser[]> {
   const data = await getAdUsers();
@@ -10,10 +15,25 @@ async function getAzureUsers(): Promise<AzureUser[]> {
 export default async function CommentPage() {
   const data = await getAzureUsers();
 
+  const session = await auth.api.getSession({
+    headers: await headers()
+})
+if(!session) {
+    return <div>Not authenticated</div>
+}
+  const email = session?.user?.email ?? "";
+
+  // Get user profile
+  const profile = preloadedQueryResult(
+    await preloadQuery(api.profile.getByEmail, { email })
+  );
+  const grades = await getGrades();
+  const subjects = await getSubjects();
+  
   const users = data.map((user) => ({
     value: user.id,
     label: user.displayName,
   }));
 
-  return <CommentForm users={users} />;
+  return <CommentForm profile={profile} grades={grades} subjects={subjects} users={users} />;
 }

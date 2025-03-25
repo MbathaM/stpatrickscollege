e2f4d28d9af8d2b1194945b5d973ca77566ba212
MbathaM <33201125+MbathaM@users.noreply.github.com>
2025-03-25 11:25:52 +0200

    Update page.tsx

--- app/(secured)/dashboard/notes/page.tsx
+++ app/(secured)/dashboard/notes/page.tsx
@@ -1,6 +1,12 @@
+import { api } from "@/convex/_generated/api";
 import NotesPageClient from "./page-client";
+import {
+    preloadQuery,
+    preloadedQueryResult,
+  } from "convex/nextjs";
 
-export default function NotesPage() {
-    
- return <NotesPageClient />
+export default async function NotesPage() {
+
+    const notes = preloadedQueryResult(await preloadQuery(api.notes.list));
+ return <NotesPageClient notes={notes}/>
 }
